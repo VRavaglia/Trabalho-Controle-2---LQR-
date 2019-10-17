ee9a7b67ec280d0897e12644edf05849f0c049a2
Victor Raposo <victor.ravaglia@hotmail.com>
2019-10-17 11:58:20 -0300

    Alteração no simulink principal

--- init.m
+++ init.m
@@ -1,17 +1,20 @@
-%Equivalencia de funcoes:
-%k[n] - ganhoObs()
-%s[n] - riccati()
-%pbarra[n+1] - pbarra()
-%p - pnaobarra()
 
-N = 5;
+
+N = 10;
 q0 = 1;
 Q = [1 1;1 1];
 phi = 1;
 gama = 1;
-s = zeros(1,N);
+c = 1;
+s = zeros(1,N+1);
+k = zeros(1, N);
+p0 = 1;
+x0 = 0;
+R = 1;
 
-%Nao eficiente
-for i = 1:N
-    s(i) = riccati(q0, Q ,phi, gama, i, N);
+s(N + 1) = q0;
+for i = N:-1:1
+    s(i) = phi*s(i + 1)*phi + Q(1,1) - phi^2*gama^2*s(i + 1)^2/(gama^2*s(i + 1) + Q(2,2));
+    k(i) = phi*gama*s(i + 1)/(Q(2,2) + gama^2*s(i + 1));
 end
+
--- riccati1.m
+++
@@ -1,8 +0,0 @@
-function s = riccati1(q0, Q ,phi, gama, k, N)
-    if k == N
-        s = q0;
-        return
-    end
-    s1 = riccati(q0, Q, phi, gama, k+1, N);
-    s = phi'*s1*phi + Q(1,1) - (phi'*s1*gama + Q(1,2))*(gama'*s1*gama+Q(2,2))^(-1)*(gama'*s1*phi+Q(1,2)');
-end
