45fe268960f16dcb0e3f516033cca3348d19a267
tSimoes06 <tiago.scr06@gmail.com>
2019-11-03 23:20:21 -0200

    Troquei um q(2,1) por q(1,2)'

--- initTemp.m
+++ initTemp.m
@@ -23,8 +23,8 @@ l = cell(1,N+1);
 % Riccati
 s{N+1} = q0;
 for k = N:-1:1
-    s{k} = phi'*s{k + 1}*phi + Q{1,1} - (phi'*s{k + 1}*gama + Q{1,2})*(gama'*s{k + 1}*gama + Q{2,2})^(-1)*(gama'*s{k + 1}*phi + Q{2,1});
-    l{k} = (Q{2,2} + gama'*s{k + 1}*gama)^(-1)*(gama'*s{k + 1}*phi - Q{2,1}); % tinha um trasnposto a mais
+    s{k} = phi'*s{k + 1}*phi + Q{1,1} - (phi'*s{k + 1}*gama + Q{1,2})*(gama'*s{k + 1}*gama + Q{2,2})^(-1)*(gama'*s{k + 1}*phi + Q{1,2}');
+    l{k} = (Q{2,2} + gama'*s{k + 1}*gama)^(-1)*(gama'*s{k + 1}*phi - Q{1,2}'); % tinha um trasnposto a mais
 end
 
 % Riccati estatico
