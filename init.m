dc0ddc5fe8187d52853c40a8f6b62f7c0f3b592a
Victor Raposo <victor.ravaglia@hotmail.com>
2019-11-05 08:51:45 -0300

    Mudanças feitas durante a aula

--- init.m
+++ init.m
@@ -1,6 +1,5 @@
 %%Sistema
-syms s
-
+syms s real
 
 %Definindo os polos
 Zeros = 1;
@@ -9,10 +8,13 @@ func_transferencia = tf(Zeros, Polos);
 
 [A, B, C, D] = tf2ss(Zeros,Polos);
 
+x0 = [1;1];
+
 
 %Discretizando
 % h = 0.01;
-h = 0.1;
+h = 0.01;
+tmax = 100;
 
 Gd = c2d(func_transferencia, h, 'zoh');% metodo ZOH de discretizacao
 [num, den] = tfdata(Gd, 'v');
@@ -23,11 +25,13 @@ Gd = c2d(func_transferencia, h, 'zoh');% metodo ZOH de discretizacao
 % Q1 = [ 1 1 ; 1 1]
 % Q12 = [ 1 ; 1]
 % Q2 = [1]
-Q1 = [ 1 1 ; 1 1]
-Q12 = [ 1 ; 1]
-Q2 = [1]
+Q1 = [ 1 0 ; 0 1];
+Q12 = [ 0 ; 0];
+Q2 = [1];
 Q = [Q1 Q12 ; Q12' Q2]; % Q agora é 3x3
 q0 = eye(length(phi));
 N = 100;
 s = cell(1,N+1);% troquei os zeros por celulas
 l = cell(1,N);
+
+riccati1;
